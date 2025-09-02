cbd3ee6bfdbaec5f17108197e238f80c0b7900a1
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2025-09-02 22:15:37 +1000

    add activity table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,12 @@ import usePaliaTime from "./hooks/usePaliaTime";
 import type { PaliaActivity } from "./data";
 import ScheduleTable from "./components/ScheduleTable";
 import useNotificationPermission from "./hooks/useNotificationPermission";
+import ActivityTable from "./components/ActivityTable";
 
 export default function Home() {
   const {permissionStatus} = useNotificationPermission();
   const {paliaTime12Hour, paliaTime24Hour, paliaCurrentHour} = usePaliaTime();
-  const [schedule, setSchedule] = useState<Array<PaliaActivity> | null>([{
-    id: 1,
-    name: "Flow-Tree Garden",
-    desc: "Find the Flow-Tree Garden in Bahari Bay. Make sure to let others know where it is!",
-    startHour: 7,
-    endHour: null 
-  }]);
+  const [schedule, setSchedule] = useState<Array<PaliaActivity> | null>(null);
   const [notifiedHour, setNotifiedHour] = useState<number | null>(null);
 
   useEffect(() => {
@@ -24,16 +19,21 @@ export default function Home() {
     if (permissionStatus !== "granted") {
       return;
     }
+    
+    // only notify if user has a schedule, palia time has loaded, and hasn't been notified already
+    if (!schedule || !paliaCurrentHour || paliaCurrentHour === notifiedHour) return;
 
-    // check if there's any activity on the user's schedule that's starting in the next hour 
-    // and hasn't been notified already
-    if (schedule && paliaCurrentHour && paliaCurrentHour !== notifiedHour) {
-      for (let i = 0; i < schedule.length; i++) {
-        if (schedule[i].startHour === paliaCurrentHour + 1) {
-          new Notification(`${schedule[i].name}`, { body: `${schedule[i].desc}` });
-          setNotifiedHour(paliaCurrentHour);
-        }
-      }
+    // find any activity in the schedule that is starting in an hour
+    const upcomingActivity: PaliaActivity | undefined = schedule.find(
+      (activity) => activity.startHour === (paliaCurrentHour + 1)
+    ); 
+
+    // send a notification and set the notified hour to current hour
+    if (upcomingActivity) {
+      new Notification(`${upcomingActivity.name}`, { body: `${upcomingActivity.desc}`})
+      // TODO: fix this so that the notification happens every day only once.
+      // if there's only one activity on the schedule, it doesn't get notified the next day.
+      setNotifiedHour(paliaCurrentHour);
     }
 
   }, [paliaTime24Hour, schedule])
@@ -44,7 +44,10 @@ export default function Home() {
       <h2>{paliaTime12Hour ?? "loading"}</h2>
       <h2>{paliaTime24Hour ?? "loading"}</h2>
       <hr></hr>
-      <ScheduleTable schedule={schedule} />
+      <div className="flex flex-row gap-24">
+        <ScheduleTable schedule={schedule} />
+        <ActivityTable schedule={schedule} />
+      </div>
     </div>
   );
 }
