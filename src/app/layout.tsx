1f68b0e014efee62df8042155ccd56d08078d537
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2025-09-28 11:18:04 +1000

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { dmSans } from "./fonts";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Palia Scheduler",
@@ -19,6 +20,7 @@ export default function RootLayout({
         className={`${dmSans.className} antialiased`}
       >
         <SpeedInsights />
+        <Analytics />
         {children}
       </body>
     </html>
