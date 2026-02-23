9af1d3e3cb419c87b6e660ed35a9af027edb50d4
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-02-23 22:40:19 +1000

    add search bar

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import {JSX, useEffect, useState} from "react";
+import {JSX, useState} from "react";
 import { ActivityType, PaliaActivity, ScheduledActivity } from "../data/data";
 import { allActivites } from "../data/allActivites";
 import ListItem from "./ListItem";
 import TypeSelect from "./TypeSelect";
+import { X } from "lucide-react";
 
 type ListProps = {
   schedule: Array<ScheduledActivity>;
@@ -15,6 +16,8 @@ type ListProps = {
 export default function List({schedule, toggleScheduleItem, toggleModal}: ListProps): JSX.Element {
   // handle state of type of activities that should be displayed in the list
   const [typeToDisplay, setTypeToDisplay] = useState<ActivityType>("Events");
+  // handle state of search input
+  const [searchInput, setSearchInput] = useState<string>("");
 
   // handle type button click
   const handleClick = (activityType: ActivityType) => {
@@ -23,9 +26,21 @@ export default function List({schedule, toggleScheduleItem, toggleModal}: ListPr
     })
   };
 
+  // handle search input
+  const handleSearchInput = (string: any) => {
+    setSearchInput(() => {
+      return string;
+    })
+  };
+
+  // clear search input
+  const handleClearSearchInput = () => {
+    setSearchInput("");
+  };
+
   // show all activities that are the same type as the one selected in alphabetical order
   const activitiesToDisplay: Array<PaliaActivity> = allActivites.filter((activity: PaliaActivity) => {
-    if (typeToDisplay === activity.type) {
+    if (typeToDisplay === activity.type && activity.name.toLowerCase().includes(searchInput.toLowerCase())) {
       return activity;
     }
   }).sort((a, b) => a.name.localeCompare(b.name)
@@ -44,8 +59,27 @@ export default function List({schedule, toggleScheduleItem, toggleModal}: ListPr
 
   return (
     <div className="flex flex-col gap-4 w-1/3">
-      <TypeSelect typeToDisplay={typeToDisplay} handleClick={handleClick} />
-      <div className="space-y-2 max-h-200 overflow-y-auto scroll-smooth pr-2 pb-4">
+      <div className="flex flex-row gap-8">
+        <TypeSelect typeToDisplay={typeToDisplay} handleClick={handleClick} />
+        <div className="relative w-full">
+          <input 
+            className="w-full h-full border-b outline-none" 
+            placeholder={`Search ${typeToDisplay}...`} 
+            type="text" 
+            value={searchInput}
+            onChange={(e) => {handleSearchInput(e.target.value)}}>
+          </input>
+          {
+            searchInput &&
+            <button 
+              className="absolute right-2 self-center hover:cursor-pointer" 
+              onClick={() => {handleClearSearchInput()}}>
+                <X size={16} />
+            </button>
+          }
+        </div>
+      </div>
+      <div className="space-y-2 max-h-200 overflow-y-auto scroll-smooth pb-4">
         {activityElements}
       </div>
     </div>
